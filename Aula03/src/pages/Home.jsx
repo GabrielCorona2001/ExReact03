738074cf50aee0c6853af5d6b7a4b0ffbe2f9ccf
GABRIEL CORONA NUNES <gabriel_nunes6@estudante.sesisenai.org.br>
2023-11-27 21:05:52 -0300

    commit

---
+++ Aula03/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import { Body } from "../components/body/Body.jsx"
+import { Footer } from "../components/footer/Footer.jsx"
+
+export function Home() {
+
+    return (
+
+        <>
+            <Body></Body>
+            <Footer></Footer>
+        </>
+    )
+}
